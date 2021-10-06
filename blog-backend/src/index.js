d137284dd93b08e6e54ec98d4090762560ec8bdd
Paolo Chang <chanhwan.chang@gmail.com>
2021-10-06 11:11:24 -0400

    Connect MongoDB

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log(`Connected to MongoDB`);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const api = require('./api');
 
@@ -16,6 +29,7 @@ app.use(bodyParser());
 // app instance에 router 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
