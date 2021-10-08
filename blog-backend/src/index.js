2f4a740532da65c7cb553f2cc25e652a4b53c8df
Paolo Chang <chanhwan.chang@gmail.com>
2021-10-08 17:05:47 -0400

    Add koa-morgan for logging

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+const fs = require('fs');
 import Koa from 'koa';
+import morgan from 'koa-morgan';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
@@ -16,15 +18,21 @@ mongoose
     console.error(e);
   });
 
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(__dirname + '/../access.log', {
+  flags: 'a',
+});
+
 const app = new Koa();
 const router = new Router();
 
-// router 설정
-router.use('/api', api.routes());
-
 // router 적용 전에 bodyParser 적용
 app.use(bodyParser());
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
 
+// router 설정
+router.use('/api', api.routes());
 // app instance에 router 적용
 app.use(router.routes()).use(router.allowedMethods());
 
