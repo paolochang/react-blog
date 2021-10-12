1519caa580e354c97a52bbfdb8de0b26547e5d16
Paolo Chang <chanhwan.chang@gmail.com>
2021-10-11 22:27:42 -0400

    Add user register

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -1,5 +1,47 @@
+import Joi, { exist } from 'joi';
+import User from '../../models/user';
+
+/**
+ * POST /api/auth/register
+ * {
+ *  username: 'velopert'
+ *  password: 'mypass123'
+ * }
+ */
 export const register = async (ctx) => {
-  // 회원가입
+  // Verify Request Body
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required(),
+  });
+
+  const result = schema.validate(ctx.request.body);
+
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { username, password } = ctx.request.body;
+
+  try {
+    // check if username exist
+    const exists = await User.findByUsername(username);
+
+    if (exists) {
+      ctx.status = 409; // Conflict
+      return;
+    }
+
+    const user = new User({ username });
+    await user.setPassword(password);
+    await user.save();
+
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const login = async (ctx) => {
