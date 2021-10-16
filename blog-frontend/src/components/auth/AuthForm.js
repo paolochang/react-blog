905d89d3d7f432d65eb159921f68fdbb97152451
Paolo Chang <chanhwan.chang@gmail.com>
2021-10-16 18:20:01 -0400

    Add AuthForm and AuthTemplate

---
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AuthFormBlock = styled.div``;
+
+/**
+ * Display registeration or login form
+ */
+
+const AuthForm = () => {
+  return <AuthFormBlock>AuthForm</AuthFormBlock>;
+};
+
+export default AuthForm;
--- blog-frontend/src/pages/LoginPage.js
+++ blog-frontend/src/pages/LoginPage.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import AuthForm from '../components/auth/AuthForm';
+import AuthTemplate from '../components/auth/AuthTemplate';
 
 const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      Login
+      <AuthTemplate>
+        <AuthForm />
+      </AuthTemplate>
+    </div>
+  );
 };
 
 export default LoginPage;
