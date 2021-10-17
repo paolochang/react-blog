84d5760e4dba458b4c94b69d679e4263dffb4de3
Paolo Chang <chanhwan.chang@gmail.com>
2021-10-17 16:46:20 -0400

    Apply state management on RegisterForm

---
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm } from '../../modules/auth';
+import AuthForm from '../../components/auth/AuthForm';
+
+const RegisterForm = () => {
+  const dispatch = useDispatch();
+  const { form } = useSelector(({ auth }) => ({ form: auth.register }));
+
+  useEffect(() => {
+    dispatch(initializeForm('register'));
+  }, [dispatch]);
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(changeField({ form: 'register', key: name, value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <AuthForm
+      type="register"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+export default RegisterForm;
