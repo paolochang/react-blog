11ca2f791115eb7d65649d4d01863c1c9a5d98e3
Paolo Chang <chanhwan.chang@gmail.com>
2021-10-22 00:34:12 -0400

    Add Editor component

---
+++ blog-frontend/src/components/write/Editor.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.bubble.css';
+import palette from '../../lib/styles/palette';
+import styled from 'styled-components';
+import Responsive from '../common/Responsive';
+
+const EditorBlock = styled(Responsive)`
+  /* 페이지 위 아래 여백 지정 */
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+`;
+const TitleInput = styled.input`
+  font-size: 3rem;
+  outline: none;
+  padding-bottom: 0.5rem;
+  border: none;
+  border-bottom: 1px solid ${palette.gray[4]};
+  margin-bottom: 2rem;
+  width: 100%;
+`;
+const QuillWrapper = styled.div`
+  /* 최소 크기 지정 및 padding 제거 */
+  .ql-editor {
+    padding: 0;
+    min-height: 320px;
+    font-size: 1.125rem;
+    line-height: 1.5;
+  }
+  .ql-editor.ql-blank::before {
+    left: 0px;
+  }
+`;
+
+const Editor = () => {
+  const quillElement = useRef(null);
+  const quillInstance = useRef(null);
+
+  useEffect(() => {
+    quillInstance.current = new Quill(quillElement.current, {
+      theme: 'bubble',
+      placeholder: 'Add content...',
+      modules: {
+        // For more options
+        // https://quilljs.com/docs/modules/toolbar/ 참고
+        toolbar: [
+          [{ header: '1' }, { header: '2' }],
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['blockquote', 'code-block', 'link', 'image'],
+        ],
+      },
+    });
+  }, []);
+
+  return (
+    <EditorBlock>
+      <TitleInput placeholder="Add title..." />
+      <QuillWrapper>
+        <div ref={quillElement} />
+      </QuillWrapper>
+    </EditorBlock>
+  );
+};
+
+export default Editor;
